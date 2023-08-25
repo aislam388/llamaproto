d2672ed44dcd71d2120d6d4317934982224d73e8
Amna Islam <amna.islam@ucp.com>
2023-08-26 01:32:46 +0500

    changed llama

--- llama.py
+++ llama.py
@@ -22,45 +22,47 @@ TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
 # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
 ############################################################################
 
+def get_response(prompt):
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
 
+    metadata = (('authorization', 'Key ' + PAT),)
 
-channel = ClarifaiChannel.get_grpc_channel()
-stub = service_pb2_grpc.V2Stub(channel)
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 
-metadata = (('authorization', 'Key ' + PAT),)
+    response = ""
 
-userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
-
-post_workflow_results_response = stub.PostWorkflowResults(
-    service_pb2.PostWorkflowResultsRequest(
-        user_app_id=userDataObject,  
-        workflow_id=WORKFLOW_ID,
-        inputs=[
-            resources_pb2.Input(
-                data=resources_pb2.Data(
-                    text=resources_pb2.Text(
-                        url=TEXT_FILE_URL
+    post_workflow_results_response = stub.PostWorkflowResults(
+        service_pb2.PostWorkflowResultsRequest(
+            user_app_id=userDataObject,  
+            workflow_id=WORKFLOW_ID,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        text=resources_pb2.Text(
+                            raw=prompt
+                        )
                     )
                 )
-            )
-        ]
-    ),
-    metadata=metadata
-)
-if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
-    print(post_workflow_results_response.status)
-    raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
+        print(post_workflow_results_response.status)
+        return response
+        #raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
 
 # We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
-results = post_workflow_results_response.results[0]
+    results = post_workflow_results_response.results[0]
 
 # Each model we have in the workflow will produce one output.
-for output in results.outputs:
-    model = output.model
+    for output in results.outputs:
+        model = output.model
 
-    print("Predicted concepts for the model `%s`" % model.id)
-    for concept in output.data.concepts:
-        print("	%s %.2f" % (concept.name, concept.value))
+        print("Predicted concepts for the model `%s`" % model.id)
+        for concept in output.data.concepts:
+            print("	%s %.2f" % (concept.name, concept.value))
 
 #Uncomment this line to print the full Response JSON
-print(results)
+    print(response)
